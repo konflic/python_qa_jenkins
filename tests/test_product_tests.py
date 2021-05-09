1a4293945d94242b5f31a373769f74ee8b1193bd
Mikhail C <44262112+konflic@users.noreply.github.com>
2021-05-09 14:31:22 +0300

    Update test_product_tests.py

--- tests/test_product_tests.py
+++ tests/test_product_tests.py
@@ -4,5 +4,5 @@ import allure
 @allure.feature("Comparison")
 @allure.title("Adding product to comparison")
 def test_add_product_to_comparison(browser):
-    allure.attach(name="screenshot", body=browser.get_screenshot_as_png(), attachmnet_type=allue.attachment_type.PNG)
+    allure.attach(name="screenshot", body=browser.get_screenshot_as_png(), attachment_type=allure.attachment_type.PNG)
     raise AssertionError("This test fails!")
